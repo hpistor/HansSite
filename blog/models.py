f3e1e84ba6687a8425d49e3e143ba62f68df1c07
Hans Pistor <hpistor@stetson.edu>
2019-01-09 14:16:56 -0500

    init

---
+++ blog/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class Entry(models.Model):
+    title = models.CharField(max_length = 500)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True, editable = False)
+    modified_at = models.DateTimeField(auto_now = True, editable = False)
+
