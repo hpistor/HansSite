069aa8e0d4530ab949046e3f6cff28f651718115
Hans Pistor <hpistor@stetson.edu>
2019-01-09 23:15:14 -0500

    move to vm

--- blog/models.py
+++ blog/models.py
@@ -9,3 +9,9 @@ class Entry(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, editable = False)
     modified_at = models.DateTimeField(auto_now = True, editable = False)
 
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = "entries"
+
--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from blog.models import Entry
 
 # Create your tests here.
+
+class EntryModelTest(TestCase):
+
+    def setUp(self):
+        self.entry = Entry(title="entry title")
+
+    def test_string_representation(self):
+        self.assertEqual(str(self.entry), self.entry.title)
+
+    def test_verbose_name_plural(self):
+        self.assertEqual(str(Entry._meta.verbose_name_plural), "entries")
+
+class ProjectTests(TestCase):
+
+    def test_homepage(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
